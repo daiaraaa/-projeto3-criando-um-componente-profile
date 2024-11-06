07a74a7e4ac2fb509a0c3ad0cd75311dcc5e437e
Daiara Barroso <160050745+daiaraaa@users.noreply.github.com>
2024-11-05 21:25:05 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Profile from './components/Profile'
+
+export default function App() {
+  return( 
+        <div>
+          <Profile 
+           img={'./eu.jpeg'}
+           name={'Daiara Barroso'} 
+           bio={'Estudante de Desenvolvimento de Sistemas'} 
+           contact={'+ 55 (85) 99433-4741'} 
+           email={'daiarabarroso09@gmail.com'} 
+           githubUrl={'https://github.com/daiaraaa'} 
+           instagramUrl={'https://instagram.com/daiarabarroso'} 
+           letterboxdUrl={'https://letterboxd.com/Daiaraa'}
+           />
+        </div> 
+   
+  )
+}
---
+++ src/components/Profile/index.jsx
@@ -0,0 +1,21 @@
+import styles from './styles.module.css'
+
+export default function Profile(props){
+    return(
+        <div className={styles.bloco}>
+            <img src={props.img} />
+            <h1 className={styles.name}>{props.name}</h1>
+            <hr />
+            <p className={styles.bio} >{props.bio}</p>
+            <hr />
+            <p className={styles.contact}>{props.contact}</p>
+            <hr />
+            <p className={styles.email}>{props.email}</p>
+            < >
+            <button><a href={props.githubUrl}>Github</a></button>
+            <button><a href={props.instagramUrl}>Instagram</a></button>
+            <button><a href={props.letterboxdUrl}>Letterboxd</a></button>
+            </>
+        </div>
+    )
+}
